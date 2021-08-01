4aec0cd29f366ea34b3fbdcff70bb7a913fc6c42
Eric Webber <ewebber283@gmail.com>
2021-07-31 20:57:06 -0400

    completed indexeddb

--- public/js/idb.js
+++ public/js/idb.js
@@ -17,7 +17,7 @@ request.onsuccess = function(event) {
     
     if (navigator.onLine) {
       
-      // uploadPizza();
+      uploadTransaction();
     }
   };
   
@@ -38,4 +38,35 @@ function saveRecord(record) {
     transactionObjectStore.add(record);
   }
 
-  
+  getAll.onsuccess = function() {
+    // if there was data in indexedDb's store, let's send it to the api server
+    if (getAll.result.length > 0) {
+      fetch('/api/transaction', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(serverResponse => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          // open one more transaction
+          const transaction = db.transaction(['new_transaction'], 'readwrite');
+          // access the new_pizza object store
+          const transactionObjectStore = transaction.objectStore('new_transaction');
+          // clear all items in your store
+          transactionObjectStore.clear();
+
+          alert('All saved transactions has been submitted!');
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  };
+
+  window.addEventListener('online', uploadTransaction);
